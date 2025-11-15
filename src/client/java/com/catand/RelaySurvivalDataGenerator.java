f2518b349e627d1e72dbe990c790ac6b47495867
catand <3047354896@qq.com>
2025-11-15 16:04:32 +0800

    初始

---
+++ src/client/java/com/catand/RelaySurvivalDataGenerator.java
@@ -0,0 +1,11 @@
+package com.catand;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RelaySurvivalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
